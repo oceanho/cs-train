41d126a07959569d4fc2eb6c6c96765caaeb862c
Ocean <ocean@oceanho.com>
2019-02-28 16:16:27 +0800

    Init a dotnet core C# project

---
+++ console-01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10, b=20;
+            Console.WriteLine(String.Format("Raw value:  a={0}, b={1}",a,b));
+            a ^= b;
+            b ^= a;
+            a ^= b;
+            Console.WriteLine(String.Format("Transfer Result:  a={0}, b={1}",a,b));
+        }
+    }
+}
